d6473d9a18db74e7dd84bc0333ccdcd6a2e4a06d
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-13 13:43:45 +0300

    add view home function for date and get projects and render in index.html, create index.html in teplats folder, add url path for home

--- awardo/views.py
+++ awardo/views.py
@@ -1,3 +1,18 @@
+import datetime as dt
 from django.shortcuts import render
+from .models import *
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    projects = Projects.get_projects()
+    date = dt.date.today()
+    return render(request, 'index.html', {"date": date, "projects":projects})
+    
