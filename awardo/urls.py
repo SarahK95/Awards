206604856e1fb8f537ef0ca33237e24be0564fed
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-13 16:30:58 +0300

    add url paths for searched_projects, get_project,new_project and profile for views function of each

--- awardo/urls.py
+++ awardo/urls.py
@@ -1,8 +1,17 @@
-from django.conf.urls import path, include
+from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    url(r'^$', views.home, name= 'home'),
+    url(r'^search/', views.search_projects, name='searched'),
+    url(r'^project/(\d+)', views.get_project, name='searched_projects'),
+    url(r'^new/project$', views.new_project, name='new-project'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
