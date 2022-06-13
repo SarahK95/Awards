853b2fff13b279c996d0ac15246f2a3c432aa9a5
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-13 12:31:27 +0300

    add Projects model with tile,image,description,link fields

--- awardo/models.py
+++ awardo/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from tkinter import CASCADE
 from django.db import models
 import datetime as dt
@@ -18,3 +19,19 @@ class Profile(models.Model):
     def __str__(self):
         return self.bio        
     
+    
+class Projects(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to = 'images/', blank=True)
+    descriptin = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    link = models.URLField()
+    
+    # def save_project(self):
+    #     self.save()
+        
+    # def delete_project(self):
+    #     self.delete()
+        
+            
+        
