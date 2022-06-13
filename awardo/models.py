bdf856e0b3c265d70aaa05512b75380bfabd52ff
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-13 11:49:46 +0300

    add Profile Model and its fields user bio and profile picture

--- awardo/models.py
+++ awardo/models.py
@@ -1,3 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    prof_pic = models.ImageField(upload_to = 'prof_pics/', blank=True)
+    
